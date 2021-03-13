e495667e966ffd659100d477a67a5e43b10d8d01
insrowc <80327733+insrowc@users.noreply.github.com>
2021-03-13 20:58:21 +0530

    Backend - searching, uploading file (drag & drop), filteraion

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const path = require("path");
 const multer = require("multer");
 const hbs = require("hbs");
 require("dotenv").config();
+let url = require("url");
 const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
-const fs = require("fs");
 
-// built in middleware
 const websitePath = path.join(__dirname, "./");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -17,8 +16,6 @@ app.set("view engine", "hbs");
 app.set("views", websitePath);
 app.set("view engine", "html");
 
-// schemas
-// post schema
 const postSchema = new mongoose.Schema(
   {
     userName: {
@@ -39,7 +36,6 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// notes schema
 const notesSchema = new mongoose.Schema(
   {
     user: String,
@@ -52,19 +48,15 @@ const notesSchema = new mongoose.Schema(
     size: Number,
     downloads: {
       type: Number,
-      default: 0
+      default: 0,
     },
-    uploadedAt: String
+    uploadedAt: String,
+    filePages: Number,
   },
   {
     timestamps: true,
   }
 );
-
-// notesSchema.virtual("uploadedAt").get(function () {
-//   const date = new Date();
-//   return `${date.getUTCDate()}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()} ${date.getHours()}:${date.getMinutes()}`;
-// });
 
 notesSchema.virtual("ext").get(function () {
   return this.mimetype.slice(this.mimetype.indexOf("/") + 1);
@@ -131,12 +123,21 @@ const bytesToKB = (arr) => {
 app.get("/", (req, res) => {
   res.render("index");
 });
-
+app.get("/notesinfo", (req, res) => {
+  res.render("notesinfo");
+});
+app.get("/uploadnotes", (req, res) => {
+  const { filename, filepages } = req.query;
+  res.render("upload", {
+    fileName: filename,
+    filePages: filepages,
+  });
+});
 app.get("/announcement", (req, res) => {
   PostModal.find({}, (err, result) => {
     if (err) console.log(err);
     else {
-      console.log(result[0].comments);
+      // console.log(result[0].comments);
       res.render("announcement", { posts: result });
     }
   });
@@ -156,11 +157,11 @@ app.post("/announcement", (req, res) => {
 
     // uploading post to posts collection
     post.save((err) => {
-      if (err) alert("Something wrong while uploading your post");
+      if (err) console.log("Something wrong while uploading your post");
       res.render("announcement");
     });
   } else {
-    alert("Collegename and Username can not be empty");
+    console.log("Collegename and Username can not be empty");
   }
 });
 
@@ -214,21 +215,35 @@ app.get("/upload", (req, res) => {
   });
 });
 
+app.post("/uploadnotes", (req, res) => {
+  const { filename, filepages } = req.body;
+  res.render("upload", { filename: filename, filepages: filepages });
+});
+
 app.post("/upload/notes", upload.single("notes"), (req, res) => {
   try {
+    const { fileName, filepages } = req.body;
+
+    req.file.filePages = filepages;
+    req.file.originalname = fileName;
+
     const {
       fieldname,
-      originalname,
       encoding,
+      originalname,
       mimetype,
       destination,
       filename,
       path,
       size,
+      filePages,
     } = req.file;
+
     const user = "loggedinuser";
     const date = new Date();
-    const uploadedAt = `${date.getUTCDate()}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()} ${date.getHours()}:${date.getMinutes()}`
+    const uploadedAt = `${date.getUTCDate()}/${
+      date.getUTCMonth() + 1
+    }/${date.getUTCFullYear()} ${date.getHours()}:${date.getMinutes()}`;
 
     const notes = new NotesModal({
       user,
@@ -240,15 +255,16 @@ app.post("/upload/notes", upload.single("notes"), (req, res) => {
       filename,
       path,
       size,
-      uploadedAt
+      uploadedAt,
+      filePages,
     });
 
     notes.save((err) => {
-      if (err) alert("Something wrong while uploading your post");
-      res.redirect("/upload");
+      if (err) console.log("Something wrong while uploading your post");
+      res.redirect("/studynotes");
     });
   } catch (err) {
-    res.send(400);
+    res.sendStatus(400);
   }
 });
 
@@ -256,13 +272,85 @@ app.post("/notes/download", (req, res) => {
   const { id } = req.body;
 
   NotesModal.findById(id, (err, note) => {
-    if(err) res.redirect("/");
-    note.downloads  += 1;
+    if (err) res.redirect("/");
+    note.downloads += 1;
     note.save();
   });
+});
+app.post('/notes/search', async(req, res) => {
+  const {search} = req.body;
+  const data  = await NotesModal.find({originalname: {$regex: search}});
+  res.render("study notes", {notesList: data})
+});
+app.post("/notes/filter", (req, res) => {
+  const {filter} = req.body;
+  switch(filter) {
+    case "high to low downloads":
+      NotesModal.find({}).sort([['downloads', -1]])
+      .exec((err,docs) => {
+        if(err) {
+          console.log(err);
+          res.redirect('/studynotes');
+        }
+        res.render("study notes", {notesList: docs});
+      });
+      
+      break;
+    case "low to high downloads":
+      NotesModal.find({}).sort("downloads")
+      .exec((err,docs) => {
+        if(err) {
+          console.log(err);
+          res.redirect('/studynotes');
+        }
+        res.render("study notes", {notesList: docs});
+      });
+      break;
+    case "latest to old date":
+      NotesModal.find({}).sort([["uploadedAt", -1]])
+      .exec((err,docs) => {
+        if(err) {
+          console.log(err);
+          res.redirect('/studynotes');
+        }
+        res.render("study notes", {notesList: docs});
+      });
+      break;
+    case "old to latest date":
+      NotesModal.find({}).sort("uploadedAt")
+      .exec((err,docs) => {
+        if(err) {
+          console.log(err);
+          res.redirect('/studynotes');
+        }
+        res.render("study notes", {notesList: docs});
+      });
+      break;
+    case "max to min pages":
+      NotesModal.find({}).sort([['filePages', -1]])
+      .exec((err,docs) => {
+        if(err) {
+          console.log(err);
+          res.redirect('/studynotes');
+        }
+        res.render("study notes", {notesList: docs});
+      });
+      break;
+    case "min to max pages":
+      NotesModal.find({}).sort("filePages")
+      .exec((err,docs) => {
+        if(err) {
+          console.log(err);
+          res.redirect('/studynotes');
+        }
+        res.render("study notes", {notesList: docs});
+      });
+      break;
+    default: 
+      res.redirect("/studynotes");
+  }
 });
 
 app.listen(PORT, () => {
   console.log("Listening at " + PORT);
 });
-  
